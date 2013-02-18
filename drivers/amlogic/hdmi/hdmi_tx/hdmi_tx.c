5d54645a3074c6b63ad9c58f75ac00ef40925c3d
J1nx <admin@j1nx.nl>
2013-02-18 20:06:21 +0000

    [aml] Add CEC kernel support
    [f16ref] (old)Initial start of CEC kernel support
    [f16ref] Turn off CEC Debug messages
    Update drivers/amlogic/hdmi/hdmi_tx/hdmi_cec_key.c
    [cec] Map the "Exit" key to BACK_SPACE (Up one level in XBMC) instead of escape (Return to the home screen in XBMC).

--- drivers/amlogic/hdmi/hdmi_tx/hdmi_tx.c
+++ drivers/amlogic/hdmi/hdmi_tx/hdmi_tx.c
@@ -115,7 +115,7 @@ static struct switch_dev sdev = {   // android ics switch device
 static unsigned char init_flag=INIT_FLAG_POWERDOWN;
 static unsigned char init_powermode=0x80;
 #else
-static unsigned char init_flag=0;
+static unsigned char init_flag=INIT_FLAG_CEC_FUNC;
 extern unsigned char uboot_vmode_flag = 0;
 static unsigned char init_powermode=0;
 #endif
@@ -1033,6 +1033,7 @@ hdmi_task_handle(void *data)
 
         if (hdmitx_device->hpd_event == 1)
         {
+	    printk("hdmi_task_handle\n");
 #ifdef HDMI_SINK_NO_EDID
             msleep(500);
 #endif
@@ -1041,11 +1042,7 @@ hdmi_task_handle(void *data)
                 hdmitx_edid_clear(hdmitx_device);
                 hdmitx_edid_parse(hdmitx_device);
                 {
-                    static unsigned flag = 1;
-                    if(flag){
-                        flag = 0;
-                        cec_node_init(hdmitx_device);
-                    }
+                   cec_node_init(hdmitx_device);
                 }
                 hdmitx_cec_dbg_print("\n-------function:%s,file:%s,line:%d-----\n",__FUNCTION__,__FILE__,__LINE__);
 #ifdef HDMI_SINK_NO_EDID
